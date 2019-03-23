cd9f9d211933881182e179f8165e98b83e2f8483
kenta-okamura <kenta-okamura@cookpad.com>
2019-03-23 11:39:41 +0900

    ネストしたテーブルの実装と、検索の実装

--- 1_SimpleExample/SimpleExample/Model.swift
+++ 1_SimpleExample/SimpleExample/Model.swift
@@ -1,19 +1,32 @@
 import Cocoa
 
+class Branch: NSObject {
+    @objc var name: String
+    init(_ name: String) {
+        self.name = name
+    }
+}
+
+extension Branch {
+    static var Master = Branch("master")
+    static var Develop = Branch("develop")
+    static var Integration = Branch("integration")
+    static var iOS12 = Branch("iOS12")
+}
+
 public class Developer: NSObject {
-    @objc var username: String = ""
+    @objc var username: String = "no name"
     @objc var ticketCount: Int = 0
-    
-    init(_ username: String, ticketCount: Int) {
+    @objc var branches: [Branch] = [.Master]
+
+    init(_ username: String, ticketCount: Int, branches: [Branch] = [.Master]) {
         self.username = username
         self.ticketCount = ticketCount
+        self.branches = branches
         super.init()
     }
 
     override init() {
-        username = "no name"
-        ticketCount = 0
-
         super.init()
     }
 }
@@ -22,12 +35,13 @@ public struct Model {
     static var developers: [Developer] {
         return [
             Developer("Staltz", ticketCount: 5),
-            Developer("Chand", ticketCount: 7),
-            Developer("Da luz", ticketCount: 3),
-            Developer("Sismanidis", ticketCount: 2),
+            Developer("Chand", ticketCount: 7, branches: [.Master, .Develop, .iOS12]),
+            Developer("Da luz", ticketCount: 3, branches: [.Develop, .Integration]),
+            Developer("Sismanidis", ticketCount: 2, branches: [.iOS12]),
             Developer("Montonen", ticketCount: 4),
-            Developer("Usalj", ticketCount: 3),
-            Developer("Röhre", ticketCount: 42)
+            Developer("Usalj", ticketCount: 3, branches: [.Integration]),
+            Developer("Röhre", ticketCount: 42),
         ]
     }
 }
+
